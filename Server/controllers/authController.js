4642237d6ec074b23507b2c6de7076305c67a830
sunilrathod098 <sunilrajputhrathod@gmail.com>
2025-05-13 01:11:32 +0530

    `feat: updated authController.js and cloudinary.js updates, added registerUser function, updated settings.json and .env files`

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -1,7 +1,9 @@
 import jwt from 'jsonwebtoken';
-import {User} from '../models/User.js';
-import {ApiError} from '../utils/ApiError.js';
-import {asyncHandler} from '../utils/asyncHandler.js'
+import { User } from '../models/User.js';
+import { ApiError } from '../utils/ApiError.js';
+import { asyncHandler } from '../utils/asyncHandler.js'
+import { uploadOnCloud } from '../utils/cloudinary.js';
+import { ApiResponse } from '../utils/ApiResponse.js';
 
 
 
@@ -11,7 +13,7 @@ const generateAccessTokenRefreshToken = async (userId) => {
         const accessToken = user.generateAccessToken();
         const refreshToken = user.generateRefreshToken();
         user.refreshToken = refreshToken
-        await user.save({ validateBeforeSave: false})
+        await user.save({ validateBeforeSave: false })
         return { accessToken, refreshToken }
     } catch (error) {
         throw new ApiError(500, `Something went wrong while generating tokens: ${error.message}`)
@@ -21,11 +23,50 @@ const generateAccessTokenRefreshToken = async (userId) => {
 
 //user register
 const registerUser = asyncHandler(async (req, res) => {
-    const {username, email, password} = req.body;
-    
-})
+    const { username, email, password, isOnline } = req.body;
+
+    //check validation
+    if ([username, email, password, isOnline].some((field) => field?.trim() === "")) { throw new ApiError(400, "All fields are required"); }
+
+    //here we cheek user is existed or not
+    const existingUser = await User.findOne(
+        { $or: [{ username }, { email }] })
+    if (existingUser) {
+        throw new ApiError(400, "User is already existed")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0].path.replace(/\\/g, "/");
+    const avatar = await uploadOnCloud(avatarLocalPath);
+    if (!avatar) {
+        throw new ApiError(500, "Failed to upload avatar");
+    }
+
+    const user = await User.create({
+        username,
+        email,
+        password,
+        isOnline,
+        avatar: {
+            public_id: avatar.public_id,
+            url: avatar.secure_url
+        },
+    });
+
+    const createdUser = await User.findById(user._id).select("-password -refreshToken");
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering user")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            "User registered successfully",
+            createdUser
+        ));
+});
+
 
 
 export {
-    generateAccessTokenRefreshToken
+    generateAccessTokenRefreshToken, registerUser
 }
--- Server/utils/cloudinary.js
+++ Server/utils/cloudinary.js
@@ -1,1 +1,47 @@
- 
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+//configure on cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+//upload image and videos in cloudinary function
+const uploadOnCloud = async (localfilePath) => {
+    try {
+        if (!localfilePath) return null
+        //upload the   file on cloudinary
+        const fileResponse = await cloudinary.uploader.upload(localfilePath, {
+            resource_type: "auto",
+        })
+        
+        //file has been uploaded successfully
+        fs.unlinkSync(localfilePath)
+        return fileResponse;
+
+    } catch (error) {
+        // Remove the local temporary file if the upload fails
+        fs.unlinkSync(localfilePath);
+
+        // console.error("Error uploading to Cloudinary: ", error);
+        return null;
+    }
+}
+
+
+//delete image and videos files from cloudinary function
+const deleteFromCloud = async (publicId, resourceType = "image") => {
+    try {
+        const fileResponse = await cloudinary.uploader.destroy(publicId, {
+            resource_type: resourceType
+        });
+        return fileResponse;
+    } catch (error) {
+        console.log("Error deleting asset from cloud: ", error.message);
+        return null;
+    }
+}
+
+export { deleteFromCloud, uploadOnCloud };
